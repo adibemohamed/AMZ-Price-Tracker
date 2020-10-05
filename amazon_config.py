45967ab82c313b8e361e89c31b43cfa663909163
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-10-05 23:38:21 +0000

    Add scrap config + Chrome driver config

--- amazon_config.py
+++ amazon_config.py
@@ -1,1 +1,18 @@
-form selenium import webdriver
+from selenium import webdriver
+
+DIRECTORY = 'reports'
+NAME = 'PS4'
+CURRENCY = '$'
+MIN_PRICE = '275'
+MAX_PRICE = '650'
+FILTSERS = {
+    'min': MIN_PRICE,
+    'max': MAX_PRICE
+}
+BASE_URL = 'https://www.amazon.us'
+
+def get_chrome_web_driver(options):
+    return webdriver.Chrome('./chromedriver', chrome_options=options)
+
+def get_web_driver_options():
+    return webdriver.ChromeOptions()
